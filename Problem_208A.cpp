8138962f7b308591c566b709ae54b2e1a013d056
Lazy-Coder-here <99077894+Lazy-Coder-here@users.noreply.github.com>
2022-07-02 11:08:24 +0530

    Update Problem_208A.cpp

--- Problem_208A.cpp
+++ Problem_208A.cpp
@@ -3,49 +3,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-/*string Original(string &x){
-    int i=3;
-    int n=x.length();
-    while(i<n-3){
-        i=x.find("WUB");
-        if(i!=n){
-            x.replace(i,3," ");
-            i+=3;
-        }
-        i++;
-    }
-    if(x[0]==' ')
-        x.erase(x[0]);
-
-    for(int j=0;j<n;j++){
-        if(x[j]==' ' && x[j+1]==' ')
-            x.erase(x[j]);
-    }
-    return x;
-}*/
 int main(){
-    string x;
-    cin>>x;
+    cin.tie(0);
+	ios_base::sync_with_stdio(false);
+    string s;
+    cin>>s;
+    bool flag=1;
 
-    int i=3;
-    int n=x.length();
-    while(i<n-3){
-        i=x.find("WUB");
-        if(i!=n){
-            x.replace(i,3," ");
-            i+=3;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='W' && s[i+1]=='U' && s[i+2]=='B'){
+            i+=2;
+            if(flag==0)
+                cout<<" ";
+            continue;
         }
-        i++;
+        else{
+            flag=0;
+            cout<<s[i];
+        }
     }
-    if(x[0]==' ')
-        x.erase(x[0]);
-
-    for(int j=0;j<n;j++){
-        if(x[j]==' ' && x[j+1]==' ')
-            x.erase(x[j]);
-    }
-
-    cout<<x;
     
 
     return 0;
